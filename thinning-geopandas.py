11399b6efe58c836ad47e9845d744065f1291467
unknown <thunwal@gmail.com>
2023-11-15 21:53:41 +0100

    replace iteration with spatial join

--- thinning-geopandas.py
+++ thinning-geopandas.py
@@ -34,24 +34,29 @@ fishnet = gpd.GeoDataFrame(geometry=[Polygon([
     (x, y), (x + cell_size, y), (x + cell_size, y + cell_size), (x, y + cell_size)
 ]) for x in x_range for y in y_range])
 
-# Create an empty GeoDataFrame for the result
-result = gpd.GeoDataFrame(columns=points.columns)
+fishnet.set_crs(epsg=crs, inplace=True)
 
-# Iterate over fishnet cells
-print(f"[{datetime.now().strftime('%H:%M:%S')}] Selecting earliest occurrence per fishnet cell...")
-for index, row in fishnet.iterrows():
-    cell_geometry = row['geometry']
+# Perform a spatial join between points and fishnet using 'intersects' relationship
+# sys:1: FutureWarning: The `op` parameter is deprecated and will be removed in a future release. Please use the `predicate` parameter instead.
+print(f"[{datetime.now().strftime('%H:%M:%S')}] Joining occurrences and fishnet cells...")
+result = gpd.sjoin(points, fishnet, how="inner", op="intersects")
 
-    # Select points that intersect with the current fishnet cell
-    selected_points = points[
-        points.intersects(cell_geometry) & ~points['geometry'].isna() & ~points['observation_year'].isna()]
+# Get the point with the minimum observation_year for each fishnet cell
+# result = result.sort_values("observation_year").groupby("index_right").first()
 
-    # If there are points in the cell, find the one with the minimum observation_year
-    if not selected_points.empty:
-        min_observation_point = selected_points.loc[selected_points['observation_year'].idxmin()]
+# Group by fishnet cell and find the point with the minimum observation_year in each group
+print(f"[{datetime.now().strftime('%H:%M:%S')}] Grouping by fishnet cell...")
+result = result.groupby("index_right").apply(lambda group: group.loc[group['observation_year'].idxmin()])
 
-        # Append the selected point to the result GeoDataFrame
-        result = result.append(min_observation_point, ignore_index=True)
+# Reset the index
+result.reset_index(drop=True, inplace=True)
 
 # Save the result GeoDataFrame to the geodatabase
-result.to_file(fr"FileGDB:{path_gdb}?layer=msculpturalis_occurrence_20230925_thinned", driver="FileGDB")
+# result.to_file(fr"FileGDB:{path_gdb}?layer=msculpturalis_occurrence_20230925_thinned", driver="FileGDB")
+result.to_file("points_thinned.gpkg", layer="points_thinned", driver="GPKG")
+
+# Convert the GeoPackage file to a feature class in the geodatabase using arcpy
+arcpy.conversion.ExportFeatures(f"points_thinned.gpkg\points_thinned", fr"{path_gdb}\msculpturalis_occurrence_20230925_thinned")
+
+# Optionally, remove the GeoPackage file if no longer needed
+os.remove("points_thinned.gpkg")
