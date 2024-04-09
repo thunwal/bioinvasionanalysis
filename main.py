ff3129be2a9dced3e6e34e92d916cf2678ee9d52
unknown <thunwal@gmail.com>
2024-04-09 17:39:47 +0200

    integrated function group_points() into main script

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from thinning import thinning
 from leastcostpaths import paths
 from distacc import distacc
 from optpaths import optpaths
-from group import sensitivity_analysis, group_paths, upper_outlier_fence
+from group import group_paths, group_points, upper_outlier_fence, sensitivity_analysis
+from expansionrate import calculate_expansion_rate
 
 # PARAMETERS ------------------------------------------------------------------
 workdir_path = r"C:\Daten\Dokumente\UNIGIS\ArcGIS Projekte\p_distanzanalyse"
@@ -20,7 +21,8 @@ end_year = 2023  # year of latest observation, or last year of analysis  # tbd: 
 
 # Define dynamic parameters
 out_gpkg = os.path.join(workdir_path, f"{presence_name}_{run}.gpkg")
-out_lyr_thinned = f"{presence_name}_{run}_thinned"
+out_lyr_points_thinned = f"{presence_name}_{run}_thinned"
+out_lyr_points_thinned_grouped = f"{presence_name}_{run}_thinned_grouped"
 out_lyr_paths = f"{presence_name}_{run}_paths"
 out_lyr_paths_grouped = f"{presence_name}_{run}_paths_grouped"
 
@@ -28,19 +30,19 @@ out_lyr_paths_grouped = f"{presence_name}_{run}_paths_grouped"
 # tbd: enable skipping of the (potentially time-consuming) thinning step if GPKG is already there (read from GPKG)?
 # tbd: make sure ArcGIS workflow works with same inputs (let ArcGIS read from GeoPackage?)
 
-#presence_thinned = thinning(workdir_path, presence_name, cost, run, year_field)
-
 # =============================================================================
 # Option 1: scikit-image
 # =============================================================================
 
-# Read cost raster
 #print(f"[{dt.now().strftime('%H:%M:%S')}] Loading cost raster from {os.path.join(workdir_path, cost_name)}...")
 #cost = rio.open(os.path.join(workdir_path, cost_name))
+presence_thinned, cell_size = thinning(workdir_path, presence_name, cost, run, year_field)
 #paths(out_gpkg, out_lyr_paths, presence_thinned, cost, year_field, start_year, end_year)
 outlier_quantile, outlier_fence = upper_outlier_fence(out_gpkg, out_lyr_paths)
 sensitivity_analysis(workdir_path, out_gpkg, out_lyr_paths, np.arange(outlier_quantile, 1.00, 0.001))
 group_paths(out_gpkg, out_lyr_paths, out_lyr_paths_grouped, outlier_quantile)
+group_points(out_gpkg, out_lyr_points_thinned, out_lyr_paths_grouped, out_lyr_points_thinned_grouped, cell_size)
+#calculate_expansion_rate(workdir_path)
 
 # =============================================================================
 # Option 2: ArcGIS Pro
